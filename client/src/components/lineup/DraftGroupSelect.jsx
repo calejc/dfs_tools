f2bc2c26225d21f5c20f9fa2ec635a2eb988953d
Cale Cortney <thortney@mailbox.org>
2022-10-19 19:40:04 -0500

    ui dependencies

---
+++ client/src/components/lineup/DraftGroupSelect.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function DraftGroupSelect() {
+  const draftGroups = useSe
+}
--- client/src/state/draftGroups.js
+++ client/src/state/draftGroups.js
@@ -1,19 +1,43 @@
 import getSlates from "../api/getDraftGroups"
-import createDefaultReducersForAsyncThunk from "./apiBased/createDefaultReducersForAsyncThunk"
-import createInitialState from "./apiBased/createInitialState"
+import { asyncPendingReducer, asyncRejectedReducer } from "./apiBased/createDefaultReducersForAsyncThunk"
+import REQUEST_STATUS from "./apiBased/REQUEST_STATUS"
 
-export const initialState = createInitialState([])
+export const initialState = {
+  draftGroups: [],
+  selectedDraftGroup: null,
+  error: null,
+  status: REQUEST_STATUS.NOT_STARTED
+}
 
 export const fetchDraftGroupsData = createAsyncThunk(
   'fetchDraftGroupsData',
   async () => await getSlates()
 )
 
+const extraReducers = {
+  [fetchDraftGroupsData.pending]: (state) => {
+    asyncPendingReducer(state)
+  },
+  [fetchDraftGroupsData.fulfilled]: (state, action) => {
+    state.status = REQUEST_STATUS.SUCCEEDED
+    state.draftGroups = action.payload
+    state.selectDraftGroup = {}
+  },
+  [fetchDraftGroupsData.rejected]: (state, action) => {
+    asyncRejectedReducer(state, action)
+  }
+}
+
 const draftGroupsSlice = createSlice({
   name: 'draftGroups',
   initialState,
-  reducers: {},
-  extraReducers: createDefaultReducersForAsyncThunk(fetchDraftGroupsData)
+  reducers: {
+    selectDraftGroup(state, action) {
+      state.selectedDraftGroup = action.payload
+    }
+  },
+  extraReducers: extraReducers
 })
 
+export const selectDraftGroup = (draftGroupId) => draftGroupsSlice.actions.selectDraftGroup(draftGroupId)
 export const draftGroupsReducer = draftGroupsSlice.reducer
